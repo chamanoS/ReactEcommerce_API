16123efe82ac3d6f1c64628c71a05dafb7c2b6f2
Chamano Ramashia <chamano.sr@gmail.com>
2021-11-02 09:38:08 +0200

    installing packages, starting server, connecting to mongodb and using dotenv

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+dotenv.config();
 
-mongoose.connect(
-  "mongodb+srv://chamanos:sc7431560@cluster0.expj7.mongodb.net/shopUI?retryWrites=true&w=majority"
-)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful"))
   .catch((err) => {
     console.log(err);
   });
 
 
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running")
 })
