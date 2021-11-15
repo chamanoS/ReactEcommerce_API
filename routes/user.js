d5896825dfbb448f8186be7f183f4bcdcf65732a
Chamano Ramashia <chamano.sr@gmail.com>
2021-11-15 16:00:23 +0200

    adding gitignore file

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
+const { verifyToken } = require("./verifyToken");
+
 const router = require("express").Router();
 
-
+router.put("/:id", verifyToken,(req,res)=>{
+    if(req.user)
+})
 
 module.exports = router;
